23cc74b52b5cd2d80947db0737b49cf1819d03e9
build <2243168976@qq.com>
2020-09-12 13:56:12 +0800

    sdm660-common:pocketmode change los source to aosp

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -25,9 +25,15 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PACKAGE_NAME := SmartisanPocketMode
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
